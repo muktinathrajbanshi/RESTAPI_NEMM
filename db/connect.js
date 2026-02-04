660c6e9248fd701c54369b4e874aeb8d9ef8926b
unknown <muktinathrajbanshi786@gmail.com>
2026-02-04 21:40:43 +0545

    new method is used for this project

--- db/connect.js
+++ db/connect.js
@@ -3,11 +3,19 @@ const { options } = require("../routes/products");
 
 uri = "mongodb+srv://muktinathrajbanshi_db_user:0atM6dJbqn5cAUS7@muktinathapi.qrtyles.mongodb.net/MuktinathAPI?appName=MuktinathAPI";
 
-const connectDB = () => {
-    return mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+const connectDB = async () => {
+
+    try {
+        console.log("connect db");
+        await mongoose.connect(uri);
+        console.log("✅ MongoDB connected");
+        
+    } catch (error) {
+        console.error("❌ MongoDB connection error:", error.message);
+        process.exit(1);
+        
+    }
+
 };
 
 module.exports = connectDB;
