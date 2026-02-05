cdec1267d58674e79362bf18eca138a1353da21a
unknown <muktinathrajbanshi786@gmail.com>
2026-02-05 20:57:42 +0545

    Read Data from Database using Express & Mongoose

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,13 @@
 const Product = require("../model/product");
 
 const getAllProducts = async (req, res) => {
-    const myData = await Product.find({});
+    const myData = await Product.find({ name: "iphone" });
     res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async (req, res) => {
-    res.status(200).json({ msg: "I am getAllProductsTesting"})
+    const myData = await Product.find({});
+    res.status(200).json({ myData });
 };
 
 module.exports = { getAllProducts, getAllProductsTesting };
