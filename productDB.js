9298da63166ec50736f83d2fa7018f99c3ae93dc
unknown <muktinathrajbanshi786@gmail.com>
2026-02-05 20:31:14 +0545

    Wow Stored API data (JSON) in Database using Express & Mongoose

--- productDB.js
+++ productDB.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 const connectDB = require("./db/connect");
 const Product = require("./model/product");
 
+const ProductJson = require("./products.json");
+
 const start = async () => {
     try {
         await connectDB(process.env.MONGODB_URL);
+        await Product.create(ProductJson);
+        console.log("success");
+        
     } catch (error) {
         console.log(error);
         
     }
-}
+};
 
 start();
