1e1bab54ff1d5eed1cbcc12114323fa9ec98577f
unknown <muktinathrajbanshi786@gmail.com>
2026-02-05 21:06:50 +0545

    oh my god that's awesome job

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getAllProducts = async (req, res) => {
 };
 
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Product.find({});
+    const myData = await Product.find( req.query );
     res.status(200).json({ myData });
 };
 
