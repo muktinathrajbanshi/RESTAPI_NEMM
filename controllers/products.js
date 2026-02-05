04c1b1afa7b8abb6754f98f7a7346440a81a92c6
unknown <muktinathrajbanshi786@gmail.com>
2026-02-05 20:46:00 +0545

    wow that awesome job done

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,8 @@
-const getAllProducts = (req, res) => {
-    res.status(200).json({ msg: "I am getAllProducts"});
+const Product = require("../model/product");
+
+const getAllProducts = async (req, res) => {
+    const myData = await Product.find({});
+    res.status(200).json({ myData });
 };
 
 const getAllProductsTesting = async (req, res) => {
