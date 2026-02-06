6ddc54cf332dec17045b9cd04b106138223f5a27
unknown <muktinathrajbanshi786@gmail.com>
2026-02-06 21:07:53 +0545

    k khalko backend hait yar

--- controllers/products.js
+++ controllers/products.js
@@ -30,6 +30,11 @@ const getAllProducts = async (req, res) => {
         apiData = apiData.select(selectFix);   
     }
 
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+
+    let skip = (page - 1) * limit;
+
        console.log(queryObject);
 
     const myData = await apiData;
@@ -37,7 +42,9 @@ const getAllProducts = async (req, res) => {
 };
 
 const getAllProductsTesting = async (req, res) => {
-    const myData = await Product.find( req.query ).select("name company");
+    console.log(req.query);
+    
+    const myData = await Product.find( req.query );
 
     res.status(200).json({ myData });
 };
