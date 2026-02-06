89a126829096975dec7bdb1d39e622dc478de618
unknown <muktinathrajbanshi786@gmail.com>
2026-02-06 07:51:29 +0545

    Adding company filter in API & Make API work better

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,17 @@
 const Product = require("../model/product");
 
 const getAllProducts = async (req, res) => {
-    const myData = await Product.find( req.query );
+
+    const { company } = req.query;
+    const queryObject = {};
+
+    if (company) {
+        queryObject.company = company;
+        console.log(queryObject);
+        
+    }
+
+    const myData = await Product.find( queryObject );
     res.status(200).json({ myData });
 };
 
