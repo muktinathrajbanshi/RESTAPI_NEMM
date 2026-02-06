04cbac02c64d9e318cf1c3e07ed1475b9fc0b0cc
unknown <muktinathrajbanshi786@gmail.com>
2026-02-06 08:01:38 +0545

    Adding Advance Search Functionality in our Rest API

--- controllers/products.js
+++ controllers/products.js
@@ -2,14 +2,22 @@ const Product = require("../model/product");
 
 const getAllProducts = async (req, res) => {
 
-    const { company } = req.query;
+    const { company, name, featured } = req.query;
     const queryObject = {};
 
     if (company) {
         queryObject.company = company;
-        console.log(queryObject);
-        
     }
+
+    if (featured) {
+        queryObject.featured = featured;
+    }
+
+    if (name) {
+        queryObject.name = { $regex : name, $options: "i" };
+    }
+
+       console.log(queryObject);
 
     const myData = await Product.find( queryObject );
     res.status(200).json({ myData });
